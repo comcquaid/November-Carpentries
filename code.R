c3e3679f57ff22cb2ac56a39ed2d4508be8e6c3f
Cora McQuaid <comcquaid@ucsd.edu>
2025-11-05 09:48:37 -0800

    compare R and Python

--- code.R
+++ code.R
@@ -1,3 +1,7 @@
 
 x <- 10
+y <- x + 3
+z <- y
 
+#idea for analysis
+z + y + x
--- code.py
+++ code.py
@@ -1,6 +1,10 @@
+
 x = 5
 y = x + 2
 
 #note to self be sure to upate code
 
 import pandas as pd
+
+#try same analysis as in R
+x + y
